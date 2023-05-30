9d433d5d59af92ea3d9ad30327ab0b66197951dc
Will Richardson <rich1051@umn.edu>
2023-05-30 11:51:24 -0500

    Input with submit button lecture done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,6 +5,7 @@ import Clicker from '../Clicker/Clicker';
 function App() {
 
 // useState hook makes state variables (moved to Clicker.jsx)!
+    
     // const [count, setCount] = useState(0);
     
     // const handleClick = () => {
@@ -15,10 +16,19 @@ function App() {
     
     // console.log(count);
 
+    const [name, setName] = useState('Luke');
+    const [nameInput, setNameInput] = useState('');
+
+    const handleNewName = () => {
+        setName(nameInput);
+    };
+
 // RENDER COMPONENT:
     return (
         <div className="App">
-            <p>Hello! My name is Luke.</p>
+            <p>Hello! My name is {name}.</p>
+            <input placeholder='enter name' onChange={(event) => setNameInput(event.target.value)}/>
+            <button onClick = {handleNewName}>Submit</button>
             <Clicker />
             <Clicker />
             <Clicker />
